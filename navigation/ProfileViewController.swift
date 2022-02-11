ecd72762a1644f0903ed6f080f2448c3580d8576
Admin <admin@MacBook-Pro-Admin.local>
2022-02-11 14:24:36 +0300

    Выполнено второе домашнее задание

---
+++ navigation/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  navigation
+//
+//  Created by Admin on 11.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        navigationItem.title = "Профиль"
+    }
+   
+}
