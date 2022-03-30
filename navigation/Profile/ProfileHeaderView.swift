e947cf9945ca834b8dd2b2977aea09a3559020dd
Admin <admin@MacBook-Pro-Admin.local>
2022-03-30 15:49:09 +0300

    Выполнено задание Экран с логинкой

--- navigation/Profile/ProfileHeaderView.swift
+++ navigation/Profile/ProfileHeaderView.swift
@@ -9,12 +9,63 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    private let avatarImageView = UIImageView(image: UIImage(named: "cat.jpg"))
-    private let nameLabel = UILabel()
-    private let statusButton = UIButton()
-    private let statusLabel = UILabel()
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "cat.jpg"))
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 50
+        return imageView
+    }()
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        label.text = "Hipster Cat"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let statusButton: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.backgroundColor = .systemBlue
+        button.setTitle("Изменить статус", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(didTapStatusButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 12.0;
+        button.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
+        button.layer.shadowRadius = 4.0
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.masksToBounds = false
+        return button
+    }()
     private var statusText = "Waiting for something..."
-    private let textField = UITextField()
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let statusTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.textColor = .black
+        textField.backgroundColor = .white
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 12
+        textField.clipsToBounds = true
+        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 5))
+        textField.leftViewMode = .always
+        textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        return textField
+    }()
     private var textFieldHeightAnchor: NSLayoutConstraint?
     private var textFieldIsHidden = true
     
@@ -30,60 +81,18 @@ class ProfileHeaderView: UIView {
     }
     
     private func setupSubViews() {
-        
-        avatarImageView.layer.borderWidth = 3
-        avatarImageView.layer.borderColor = UIColor.white.cgColor
-        avatarImageView.clipsToBounds = true
-        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-        avatarImageView.layer.cornerRadius = 50
         self.addSubview(avatarImageView)
-        
-        nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        nameLabel.textColor = .black
-        nameLabel.text = "Hipster Cat"
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(nameLabel)
-        
-        statusLabel.font = UIFont.systemFont(ofSize: 14)
-        statusLabel.textColor = .gray
         statusLabel.text = statusText
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(statusLabel)
-        
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.font = UIFont.systemFont(ofSize: 15)
-        textField.textColor = .black
-        textField.backgroundColor = .white
-        textField.layer.borderWidth = 1
-        textField.layer.borderColor = UIColor.black.cgColor
-        textField.layer.cornerRadius = 12
-        textField.clipsToBounds = true
-        textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
-        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 5))
-        textField.leftViewMode = .always
-        textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
-        self.addSubview(textField)
-        
-        statusButton.clipsToBounds = true
-        statusButton.backgroundColor = .systemBlue
-        statusButton.setTitle("Изменить статус", for: .normal)
-        statusButton.setTitleColor(.white, for: .normal)
-        statusButton.addTarget(self, action: #selector(didTapStatusButton), for: .touchUpInside)
-        statusButton.translatesAutoresizingMaskIntoConstraints = false
-        statusButton.layer.cornerRadius = 12.0;
-        statusButton.layer.shadowOffset = CGSize(width: 4.0, height: 4.0)
-        statusButton.layer.shadowRadius = 4.0
-        statusButton.layer.shadowColor = UIColor.black.cgColor
-        statusButton.layer.shadowOpacity = 0.7
-        statusButton.layer.masksToBounds = false
+        self.addSubview(statusTextField)
         self.addSubview(statusButton)
-        
     }
     
     func activateConstraints() {
         
         let safeArea = self.safeAreaLayoutGuide
-        self.textFieldHeightAnchor = self.textField.heightAnchor.constraint(equalToConstant: 0)
+        self.textFieldHeightAnchor = self.statusTextField.heightAnchor.constraint(equalToConstant: 0)
         
         NSLayoutConstraint.activate([
             avatarImageView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 16),
@@ -100,12 +109,12 @@ class ProfileHeaderView: UIView {
             statusLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 34),
             statusLabel.heightAnchor.constraint(equalToConstant: 14),
             
-            textField.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            textField.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
-            textField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 16),
+            statusTextField.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            statusTextField.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 16),
             textFieldHeightAnchor!,
             
-            statusButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 16),
+            statusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: 16),
             statusButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
             statusButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
             statusButton.heightAnchor.constraint(equalToConstant: 50)
@@ -121,13 +130,13 @@ class ProfileHeaderView: UIView {
         
         self.textFieldHeightAnchor?.constant = textFieldIsHidden ? 40 : 0
         if textFieldIsHidden {
-            self.textField.becomeFirstResponder()
+            self.statusTextField.becomeFirstResponder()
             self.statusButton.setTitle("Сохранить статус", for: .normal)
         } else {
             self.statusButton.setTitle("Изменить статус", for: .normal)
             self.endEditing(true)
         }
-        self.textField.text = ""
+        self.statusTextField.text = ""
         self.textFieldIsHidden.toggle()
         self.statusLabel.text = self.statusText
     }
